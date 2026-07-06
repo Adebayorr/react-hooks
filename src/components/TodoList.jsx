16bb34c49f528e9df08709e69fee7c5b960e9d22
Adebayorr <raheemadebayorr@gmail.com>
2026-07-06 19:44:37 +0100

    add styling to TodoList component

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -26,16 +26,20 @@ function TodoList () {
     }
     return (
         <div className="todo-container">
-            <input 
-                type="text" 
-                placeholder="Enter To-do"
-                value={todo}
-                onChange={addTodoItem}
-            />
-            <button 
-                onClick={UpdateTodoList}
-            >Add To-do</button>
-            <ul>
+            <div className="todo-input-container">
+                <input 
+                    type="text" 
+                    placeholder="Enter To-do"
+                    value={todo}
+                    onChange={addTodoItem}
+                    className="todo-input"
+                />
+                <button 
+                    onClick={UpdateTodoList}
+                    className="add-todo-btn"
+                >Add To-do</button>
+            </div>
+            <ul className="todo-list">
                 {list.length === 0 && <p>Todo List is Empty</p>}
                 {list.map(item => (
                     <li key={item.id}>
